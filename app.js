cecce575f75e67730e137e6cfb1202b81dc8b0a8
Felix Ortega <99614994+FelixOrtegaMejia@users.noreply.github.com>
2022-07-19 17:22:58 -0400

    Removed video save
    Videos cannot be right click saved

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-let list = document.querySelectorAll('.list');
-let itemBox = document.querySelectorAll('.itemBox');
+let list = document.querySelectorAll(".list");
+let itemBox = document.querySelectorAll(".itemBox");
 
-for(let i = 0; i<list.length; i++){
-    list[i].addEventListener('click',function(){
-        for(let j=0; j<list.length; j++){
-            list[j].classList.remove('active');
-        }
-        this.classList.add('active');
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", function () {
+    for (let j = 0; j < list.length; j++) {
+      list[j].classList.remove("active");
+    }
+    this.classList.add("active");
 
-        let categories = this.getAttribute('category');
-        for( let k = 0; k<itemBox.length; k++){
-            itemBox[k].classList.add('hide');
-            itemBox[k].classList.remove('active');
+    let categories = this.getAttribute("category");
+    for (let k = 0; k < itemBox.length; k++) {
+      itemBox[k].classList.add("hide");
+      itemBox[k].classList.remove("active");
 
-            if(itemBox[k].getAttribute('data-item') == categories ||
-            categories == "all"){
-                itemBox[k].classList.remove('hide');
-                itemBox[k].classList.add('active');
-            }
-        }
-    })
+      if (
+        itemBox[k].getAttribute("data-item") == categories ||
+        categories == "all"
+      ) {
+        itemBox[k].classList.remove("hide");
+        itemBox[k].classList.add("active");
+      }
+    }
+  });
 }
+
+video = getElementsByTagName("video");
+function removeControls(video) {
+  video.removeAttribute("controls");
+}
+window.onload = removeControls(video);
